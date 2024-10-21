a8bf56fdccd28be3e201f2581e94f2d0accd10f9
Nguyen Luan <2051052077luan@ou.edu.vn>
2024-10-21 15:54:17 +0700

    hahaha

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -24,7 +24,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> kakakaka
             </li>
             <li className="about-activity">
               <ImPointRight /> haha
